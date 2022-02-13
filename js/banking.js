f1b052b05511633bb0fb9fa2f8e4393ebbc7220f
zafaremon <abuzaforemon@gmail.com>
2022-02-13 20:13:52 +0600

    deposit js

--- js/banking.js
+++ js/banking.js
@@ -1,2 +1,21 @@
+// handle deposit button event
+document.getElementById('deposit-btn').addEventListener('click', function () {
+  // get deposited amount
+  const depositInput = document.getElementById('deposit-input');
+  // get text format
+  const newDepositText = depositInput.value;
+  // convert into number
+  const newDepositAmount = parseFloat(newDepositText);
 
+  // update deposit total
+  const depositTotal = document.getElementById('deposit-total');
+  // get text format
+  const previousDepositText = depositTotal.innerText;
+  // convert into number
+  const previousDepositAmount = parseFloat(previousDepositText);
+
+  const newDepositTotal = previousDepositAmount + newDepositAmount;
+  depositTotal.innerText = newDepositTotal;
+
+});
 
